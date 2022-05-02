c19228b9adb6d547e625a97a580c75b504c854bd
Steven <wiltse90@gmail.com>
2022-05-02 04:45:10 -0500

    Week 6 Update

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,15 @@ app.get('/api/product/:id', (req, res) => {
     res.json(product)
 })
 
+app.use('/')
+
+const __dirname = path.resolve()
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, 'frontend/build')))
+
+    app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+}
+
 const PORT = process.env.PORT || 5000
 const MODE = process.env.NODE_ENV 
 app.listen(PORT, console.log(`Server running in ${MODE} mode on port ${PORT}`))

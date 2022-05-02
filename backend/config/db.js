3bf6c872cb553ffd6fef6a483a33a7839b5b73b2
Steven <wiltse90@gmail.com>
2022-05-02 06:15:53 -0500

    Week 6 Update

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
 
-    //const url = 'mongodb://localhost:27017/applicationdb' 
-    const url = 'mongodb+srv://appuser:mongodb123@cluster0.na7lp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    const url = 'mongodb://localhost:27017/applicationdb' 
+
 
     try {
         const conn = await mongoose.connect(url)
